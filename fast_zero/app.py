e4b24dac83e6e556cb8cb8920959543807553405
willrockoliv <william.r.o.wro@gmail.com>
2024-07-10 19:35:45 -0300

    feat(api): adding reponse model Message

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,9 +2,11 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
