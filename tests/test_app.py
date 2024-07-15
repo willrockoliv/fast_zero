15c79403f1551eb749547eeb09e045b4dfc257cb
willrockoliv <william.r.o.wro@gmail.com>
2024-07-15 18:41:34 -0300

    feat(api): adding the html route

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,22 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_get_html_deve_retornar_ok_e_ola_mundo_em_html():
+    client = TestClient(app)
+    expected_response = """
+        <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+        </html>
+    """
+
+    response = client.get('/html/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == expected_response
