468adfb82d711a3b1e8e9f57869c4ed16f8da0d6
willrockoliv <william.r.o.wro@gmail.com>
2024-08-23 20:25:51 -0300

    feat(auth): adding password hashing feature

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.security import get_password_hash
 
 app = FastAPI()
 
@@ -64,7 +65,9 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
             )
 
     db_user = User(
-        username=user.username, password=user.password, email=user.email
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
     )
 
     session.add(db_user)
@@ -93,8 +96,9 @@ def update_user(
         )
 
     db_user.username = user.username
-    db_user.password = user.password
     db_user.email = user.email
+    db_user.password = get_password_hash(user.password)
+
     session.commit()
     session.refresh(db_user)
 
--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,3 +1,11 @@
 from pwdlib import PasswordHash
 
 pwd_context = PasswordHash.recommended()
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_pasword: str, hashed_password: str):
+    return pwd_context.verify(plain_pasword, hashed_password)
